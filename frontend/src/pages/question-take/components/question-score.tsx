d82fd21d3130431ac20f02f98ff4946fbfba46a2
cernr <49557131+cernr@users.noreply.github.com>
2025-09-16 16:07:03 +0200

    fix lint

--- frontend/src/pages/question-take/components/question-score.tsx
+++ frontend/src/pages/question-take/components/question-score.tsx
@@ -4,6 +4,4 @@ interface QuestionScoreProps {
     readonly score: number
 }
 
-export const QuestionScore = (props: QuestionScoreProps) => (
-    <p className="question-score">Score: {props.score}</p>
-)
+export const QuestionScore = (props: QuestionScoreProps) => <p className="question-score">Score: {props.score}</p>
--- frontend/src/pages/question-take/question-feedback-state.ts
+++ frontend/src/pages/question-take/question-feedback-state.ts
@@ -25,19 +25,23 @@ export const useQuestionFeedbackState = (state: QuestionTakeState, answers: Answ
     return { isQuestionCorrect, isAnswerCorrect, showFeedback, isUserSelected, score }
 }
 
-const calculateScore = (selectedAnswerIdxs: AnswerIdxs, correctAnswers: AnswerIdxs, isAnsweredCorrectly: boolean): number => {
-    var correctSelectedAnswers = selectedAnswerIdxs.filter(item => correctAnswers.includes(item));
-    var wrongSelectedAnswers = selectedAnswerIdxs.filter(item => !correctAnswers.includes(item));
-    var missingCorrectAnswers = correctAnswers.length - correctSelectedAnswers.length;
-    var totalErrorCount = wrongSelectedAnswers.length + missingCorrectAnswers;
+const calculateScore = (
+    selectedAnswerIdxs: AnswerIdxs,
+    correctAnswers: AnswerIdxs,
+    isAnsweredCorrectly: boolean,
+): number => {
+    const correctSelectedAnswers = selectedAnswerIdxs.filter(item => correctAnswers.includes(item))
+    const wrongSelectedAnswers = selectedAnswerIdxs.filter(item => !correctAnswers.includes(item))
+    const missingCorrectAnswers = correctAnswers.length - correctSelectedAnswers.length
+    const totalErrorCount = wrongSelectedAnswers.length + missingCorrectAnswers
 
-    if(isAnsweredCorrectly) {
-        return 1;
+    if (isAnsweredCorrectly) {
+        return 1
     }
 
-    if(totalErrorCount <= 1) {
-        return 0.5;
+    if (totalErrorCount <= 1) {
+        return 0.5
     }
 
-    return 0;
+    return 0
 }
