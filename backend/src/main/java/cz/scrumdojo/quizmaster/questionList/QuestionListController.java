c45634db5a627e667f9829079256a1d6e07e3c76
vaclavventa-ext44115 <159039664+vaclavventa-ext44115@users.noreply.github.com>
2025-09-16 16:19:28 +0200

    refactor: hardocded tests

--- backend/src/main/java/cz/scrumdojo/quizmaster/questionList/QuestionListController.java
+++ backend/src/main/java/cz/scrumdojo/quizmaster/questionList/QuestionListController.java
@@ -6,7 +6,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
