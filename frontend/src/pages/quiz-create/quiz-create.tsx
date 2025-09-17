8073d30d8941653bf0de5f21b3d7e6695ba054c8
sherpaPSX <sherpa.psx@gmail.com>
2025-09-17 09:21:20 +0200

    quiz ui lint fix

--- frontend/src/pages/quiz-create/quiz-create.tsx
+++ frontend/src/pages/quiz-create/quiz-create.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { useApi } from 'api/hooks'
 import { useState } from 'react'
-import { QuizQuestion } from 'model/quiz-question'
+import type { QuizQuestion } from 'model/quiz-question'
 import { getListQuestions } from 'api/question-list'
 import { postQuiz } from 'api/quiz'
 
@@ -14,45 +14,41 @@ export const QuizCreatePage = () => {
 
     useApi(listId, getListQuestions, setQuestionList)
 
-
     const handleSelect = (id: number) => {
-        setSelectedIds((prev) =>
-            prev.includes(id)
-                ? prev.filter((prevId) => prevId !== id)
-                : [...prev, id]
-        )
+        setSelectedIds(prev => (prev.includes(id) ? prev.filter(prevId => prevId !== id) : [...prev, id]))
     }
 
-    const handleCreateQuiz = async() => {
+    const handleCreateQuiz = async () => {
         const quizId = await postQuiz({
-            title: "TOOD",
-            description: "TODO",
+            title: 'TOOD',
+            description: 'TODO',
             timeLimit: 0,
             questionIds: selectedIds,
             afterEach: false,
-            passScore: 0
+            passScore: 0,
         })
 
         setQuizId(quizId)
     }
 
-
     return (
         <div>
             <h2>Create Quiz</h2>
-    {questionList.map((item) => (
-        <div key={String(item.id)}>
-        <input
-            id={String(item.id)}
-        type="checkbox"
-        onChange={() => handleSelect(item.id)}
-        />
-        <label htmlFor={item.hash}>{item.question}</label>
-        </div>
-    ) )}
+            {questionList.map(item => (
+                <div key={String(item.id)}>
+                    <input id={String(item.id)} type="checkbox" onChange={() => handleSelect(item.id)} />
+                    <label htmlFor={item.hash}>{item.question}</label>
+                </div>
+            ))}
 
-    <button onClick={handleCreateQuiz}>Create quiz</button>
-    {quizId && <>Quiz url: <a href={`${location.origin}/quiz/${quizId}`}>{`${location.origin}/quiz/${quizId}`}</a></>}
-    </div>
-)
+            <button onClick={handleCreateQuiz} type="button">
+                Create quiz
+            </button>
+            {quizId && (
+                <>
+                    Quiz url: <a href={`${location.origin}/quiz/${quizId}`}>{`${location.origin}/quiz/${quizId}`}</a>
+                </>
+            )}
+        </div>
+    )
 }
