0c8685c13c3a596221a186acac478226765fe196
Karel Smutny <karel.smutny@outlook.com>
2025-10-08 21:04:08 +0200

    refactor(frontend): Minor refactor

--- frontend/src/pages/create-question/validators.ts
+++ frontend/src/pages/create-question/validators.ts
@@ -3,10 +3,12 @@ import type { QuestionFormData } from './form/question-form-data'
 
 export function validateQuestionFormData(questionForm: QuestionFormData): Set<ErrorCode> {
     const errors = new Set<ErrorCode>()
-    const correctAnwerCount = questionForm.answers.filter(answer => answer.isCorrect).length
-    const emptyAnswerCount = questionForm.answers.filter(answer => answer.answer.trim() === '').length
-    const emptyExplanationCount = questionForm.answers.filter(answer => answer.explanation.trim() === '').length
-    const nonEmptyExplanationCount = questionForm.answers.filter(answer => answer.explanation.trim() !== '').length
+
+    const answers = questionForm.answers
+    const correctAnwerCount = answers.filter(answer => answer.isCorrect).length
+    const emptyAnswerCount = answers.filter(answer => answer.answer.trim() === '').length
+    const emptyExplanationCount = answers.filter(answer => answer.explanation.trim() === '').length
+    const nonEmptyExplanationCount = answers.filter(answer => answer.explanation.trim() !== '').length
 
     if (questionForm.question === '') errors.add('empty-question')
     if (emptyAnswerCount > 0) errors.add('empty-answer')
