400ee5e32eb8b341233266718357d3fe8fc7098b
Karel Smutny <karel.smutny@outlook.com>
2025-10-09 17:38:20 +0200

    fix(frontend): Fixed useApi hook

--- frontend/src/api/hooks.ts
+++ frontend/src/api/hooks.ts
@@ -1,10 +1,16 @@
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 
 export const useApi = <T>(id: string | undefined, fetch: (id: string) => Promise<T>, setData: (data: T) => void) => {
+    // React boogaloo to avoid infinite useEffect loop
+    const fetchRef = useRef(fetch)
+    const setDataRef = useRef(setData)
+    fetchRef.current = fetch
+    setDataRef.current = setData
+
     const fetchData = useCallback(async () => {
         if (id) {
-            const data = await fetch(id)
-            setData(data)
+            const data = await fetchRef.current(id)
+            setDataRef.current(data)
         }
     }, [id])
 
